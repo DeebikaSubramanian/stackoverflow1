7618c405e3e4490776210744f6e8020c1c23c990
DeebikaSubramanian <deebideebikask@gmail.com>
2022-05-05 09:16:41 +0300

    commited

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import UserReducer from '../features/UserSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    user: UserReducer,
+  }, 
 });
+
+
